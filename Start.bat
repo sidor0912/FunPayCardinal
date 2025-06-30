960f312062d6d3c4b3ee7888b7cb1d6e7b1a9322
h0rper <TAS1122@yandex.ru>
2025-06-30 16:12:42 +0300

    improved starter for check for exists of .venv

--- Start.bat
+++ Start.bat
@@ -1,1 +1,10 @@
-.venv\Scripts\python.exe main.py
+@echo off
+chcp 65001 > nul
+if EXIST .venv\Scripts\python.exe (
+    .venv\Scripts\python.exe main.py
+) else (
+    echo ====================================================
+    echo Виртуальное окружение не найдено. Пожалуйста, запустите Setup.bat сначала.
+    echo ====================================================
+    pause
+)
